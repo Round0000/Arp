3c7042eed204334853334a12a2dd564dea4d813f
Round0000 <billybinche@gmail.com>
2021-11-22 10:54:48 +0100

    commit

--- app.js
+++ app.js
@@ -1,34 +1,5 @@
 let sample = "piano";
 
-//
-let currConf = {
-  arp1: {
-    tempo: 0,
-    notes: [],
-    random: false,
-    tone: "full",
-  },
-  arp2: {
-    tempo: 0,
-    notes: [],
-    random: false,
-    tone: "full",
-  },
-  arp3: {
-    tempo: 0,
-    notes: [],
-    random: false,
-    tone: "full",
-  },
-  arp4: {
-    tempo: 0,
-    notes: [],
-    random: false,
-    tone: "full",
-  },
-};
-//
-
 function getNote(key) {
   let audio = new Audio(`/audio/${sample}/${key}.mp3`);
   return audio;
@@ -63,644 +34,140 @@ keyboard_keys.forEach((key) => {
   keys.push(key.id);
 });
 
-let scale_C_Major = [
-  keys[0],
-  keys[2],
-  keys[4],
-  keys[5],
-  keys[7],
-  keys[9],
-  keys[11],
-  keys[12],
-  keys[14],
-  keys[16],
-  keys[17],
-  keys[19],
-  keys[21],
-  keys[23],
-  keys[24],
-];
-let scale_C_Lydian = [
-  keys[0],
-  keys[2],
-  keys[4],
-  keys[6],
-  keys[7],
-  keys[9],
-  keys[11],
-  keys[12],
-  keys[14],
-  keys[16],
-  keys[18],
-  keys[19],
-  keys[21],
-  keys[23],
-  keys[24],
-];
-
-// arp
 function getRandom(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// const interval = setInterval(arp, 1000);
+arp.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let formData = new FormData(arp);
+  let units = document.querySelectorAll(".arpUnit");
+  let unitObjects = [];
+  units.forEach((unit) => {
+    let n = unit.dataset.unit;
+    let data = {
+      n: n,
+      tempo: parseInt(formData.get("arpTempo" + n)),
+      notes: formData.getAll("arpNote" + n).filter((note) => note),
+      random: formData.get("arpRandom" + n) === "true",
+    };
+    unitObjects.push(data);
+  });
+  console.log(unitObjects);
+  unitObjects.forEach((unit) => {
+    playSequence(unit);
+  });
+});
 
-function arpeggiate(scale, tempo, random, tone, selectedNotes) {
-  if (tempo < 150) {
-    return;
-  }
-  let notes = [];
+let intervals = [];
 
-  if (tone === "low") {
-    if (selectedNotes.length > 0) {
-      selectedNotes.forEach((n) => {
-        notes.push(scale.slice(0, 7)[n]);
-      });
-    } else {
-      notes = scale.slice(0, 7);
-    }
-  } else if (tone === "high") {
-    if (selectedNotes.length > 0) {
-      selectedNotes.forEach((n) => {
-        notes.push(scale.slice(7, scale.length - 1)[n]);
-      });
-    } else {
-      notes = scale.slice(7, scale.length - 1);
-    }
+function playSequence(o) {
+  console.log("unit " + o.n, o);
+  let c = 0;
+  if (o.random) {
+    let interval = setInterval(() => {
+      playNote(o.notes[getRandom(0, o.notes.length - 1)]);
+    }, o.tempo);
+    intervals.push(interval);
   } else {
-    if (selectedNotes.length > 0) {
-      selectedNotes.forEach((n) => {
-        notes.push(scale[n]);
-      });
-      selectedNotes.forEach((n) => {
-        notes.push(scale[7 + Number(n)]);
-      });
-    } else {
-      notes = scale;
-    }
-  }
-
-  if (random) {
-    setInterval(() => {
-      playNote(notes[getRandom(0, notes.length - 1)]);
-    }, tempo);
-  } else {
-    let counter = 0;
-
-    setInterval(() => {
-      playNote(notes[counter]);
-      if (counter + 1 === notes.length) {
-        counter = 0;
-      } else {
-        counter++;
+    let interval = setInterval(() => {
+      if (document.getElementById(o.notes[c])) {
+        playNote(o.notes[c]);
       }
-    }, tempo);
+      c++;
+      if (c >= o.notes.length) {
+        c = 0;
+      }
+    }, o.tempo);
+    intervals.push(interval);
   }
 }
 
-arp.addEventListener("submit", (e) => {
-  e.preventDefault();
+// Add unit
 
-  // Config save
+stopBtn.addEventListener("click", () => {
+  intervals.forEach((i) => clearInterval(i));
+});
 
-  currConf.arp1.tempo = Number(arp.arp1.value);
-  currConf.arp2.tempo = Number(arp.arp2.value);
-  currConf.arp3.tempo = Number(arp.arp3.value);
-  currConf.arp4.tempo = Number(arp.arp4.value);
-
-  if (arp.notesArray1) {
-    const userArr1 = arp.notesArray1.value.trim().split(" ");
-    userArr1.forEach((n) => {
-      currConf.arp1.notes.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp1.forEach((n) => {
-      if (n.checked) {
-        currConf.arp1.notes.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray2) {
-    const userArr2 = arp.notesArray2.value.trim().split(" ");
-    userArr2.forEach((n) => {
-      currConf.arp2.notes.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp2.forEach((n) => {
-      if (n.checked) {
-        currConf.arp2.notes.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray3) {
-    const userArr3 = arp.notesArray3.value.trim().split(" ");
-    userArr3.forEach((n) => {
-      currConf.arp3.notes.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp3.forEach((n) => {
-      if (n.checked) {
-        currConf.arp3.notes.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray4) {
-    const userArr4 = arp.notesArray4.value.trim().split(" ");
-    userArr4.forEach((n) => {
-      currConf.arp4.notes.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp4.forEach((n) => {
-      if (n.checked) {
-        currConf.arp4.notes.push(n.value);
-      }
-    });
-  }
-
-  currConf.arp1.random = arp.randomArp1.checked;
-  currConf.arp2.random = arp.randomArp2.checked;
-  currConf.arp3.random = arp.randomArp3.checked;
-  currConf.arp4.random = arp.randomArp4.checked;
-  currConf.arp1.tone = arp.toneArp1.value;
-  currConf.arp2.tone = arp.toneArp2.value;
-  currConf.arp3.tone = arp.toneArp3.value;
-  currConf.arp4.tone = arp.toneArp4.value;
-
-  console.log(currConf);
-
-  //
-
-  const notesArp1 = [];
-  const notesArp2 = [];
-  const notesArp3 = [];
-  const notesArp4 = [];
-
-  if (arp.notesArray1) {
-    const userArr1 = arp.notesArray1.value.trim().split(" ");
-    console.log(userArr1);
-    userArr1.forEach((n) => {
-      notesArp1.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp1.forEach((n) => {
-      if (n.checked) {
-        notesArp1.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray2) {
-    const userArr2 = arp.notesArray2.value.trim().split(" ");
-    console.log(userArr2);
-    userArr2.forEach((n) => {
-      notesArp2.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp2.forEach((n) => {
-      if (n.checked) {
-        notesArp2.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray3) {
-    const userArr3 = arp.notesArray3.value.trim().split(" ");
-    console.log(userArr3);
-    userArr3.forEach((n) => {
-      notesArp3.push(Number(n) - 1);
-    });
-  } else {
-    arp.notesArp3.forEach((n) => {
-      if (n.checked) {
-        notesArp3.push(n.value);
-      }
-    });
-  }
-
-  if (arp.notesArray4) {
-    const userArr4 = arp.notesArray4.value.trim().split(" ");
-    console.log(userArr4);
-    userArr4.forEach((n) => {
-      notesArp4.push(Number(n) - 1);
-    });
-    console.log(notesArp4);
-  } else {
-    arp.notesArp4.forEach((n) => {
-      if (n.checked) {
-        notesArp4.push(n.value);
-      }
-    });
-  }
-
-  console.log(notesArp1);
-  console.log(notesArp2);
-  console.log(notesArp3);
-  console.log(notesArp4);
-
-  if (arp.arp1.value >= 100) {
-    arpeggiate(
-      scale_C_Lydian,
-      arp.arp1.value,
-      arp.randomArp1.checked,
-      arp.toneArp1.value,
-      notesArp1
-    );
-  }
-  if (arp.arp2.value >= 100) {
-    arpeggiate(
-      scale_C_Lydian,
-      arp.arp2.value,
-      arp.randomArp2.checked,
-      arp.toneArp2.value,
-      notesArp2
-    );
-  }
-  if (arp.arp3.value >= 100) {
-    arpeggiate(
-      scale_C_Lydian,
-      arp.arp3.value,
-      arp.randomArp3.checked,
-      arp.toneArp3.value,
-      notesArp3
-    );
-  }
-  if (arp.arp4.value >= 100) {
-    arpeggiate(
-      scale_C_Lydian,
-      arp.arp4.value,
-      arp.randomArp4.checked,
-      arp.toneArp4.value,
-      notesArp4
-    );
+addUnitBtn.addEventListener("click", () => {
+  if (document.querySelectorAll(".arpUnit").length < 4) {
+    const newUnit = document.createElement("DIV");
+    const id = document.querySelectorAll(".arpUnit").length + 1;
+    newUnit.classList.add("arpUnit");
+    newUnit.dataset.unit = id;
+    newUnit.id = "arp" + id;
+    newUnit.innerHTML = arp1.innerHTML
+      .replace("arpTempo1", "arpTempo" + id)
+      .replaceAll("arpRandom1", "arpRandom" + id)
+      .replaceAll("arpNote1", "arpNote" + id);
+    units.appendChild(newUnit);
+    addFocusEventListeners(newUnit);
   }
 });
 
-//
-//
-//
+let currentNoteInput;
 
-let demos = [
-  {
-    arp1: {
-      tempo: 1400,
-      notes: ["0", "2", "3", "6", "4"],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 1800,
-      notes: ["0", "2", "4", "6"],
-      random: true,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 200,
-      notes: ["6", "2"],
-      random: true,
-      tone: "full",
-    },
-    arp4: {
-      tempo: 300,
-      notes: ["0", "2", "4"],
-      random: false,
-      tone: "low",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 300,
-      notes: [],
-      random: false,
-      tone: "high",
-    },
-    arp2: {
-      tempo: 150,
-      notes: ["2", "4", "6"],
-      random: true,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 900,
-      notes: ["0", "3"],
-      random: true,
-      tone: "full",
-    },
-    arp4: {
-      tempo: 0,
-      notes: [],
-      random: false,
-      tone: "",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 400,
-      notes: ["0", "1", "6", "2", "4", "4", "4", "4"],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 1000,
-      notes: ["3", "6", "0", "3", "1", "1", "1", "1"],
-      random: false,
-      tone: "low",
-    },
-    arp3: {
-      tempo: 1600,
-      notes: ["6"],
-      random: false,
-      tone: "low",
-    },
-    arp4: {
-      tempo: 600,
-      notes: ["0", "2", "3", "5", "2", "4"],
-      random: true,
-      tone: "high",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 600,
-      notes: ["1", "3", "5"],
-      random: false,
-      tone: "high",
-    },
-    arp2: {
-      tempo: 900,
-      notes: ["2", "3", "6"],
-      random: false,
-      tone: "low",
-    },
-    arp3: {
-      tempo: 900,
-      notes: ["1", "2", "4", "6"],
-      random: true,
-      tone: "high",
-    },
-    arp4: {
-      tempo: 300,
-      notes: ["0", "0", "0", "0", "3", "3", "3", "5"],
-      random: false,
-      tone: "low",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 150,
-      notes: [],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 300,
-      notes: [],
-      random: false,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 600,
-      notes: [],
-      random: false,
-      tone: "high",
-    },
-    arp4: {
-      tempo: 750,
-      notes: [],
-      random: false,
-      tone: "high",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 300,
-      notes: [0, 2, 6],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 700,
-      notes: [4, 1, 5, 3],
-      random: false,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 200,
-      notes: ["2", "7"],
-      random: false,
-      tone: "full",
-    },
-    arp4: {
-      tempo: 0,
-      notes: [-1],
-      random: false,
-      tone: "",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 250,
-      notes: [2, 3, 5, 1, 0, 6, 1, 2],
-      random: false,
-      tone: "high",
-    },
-    arp2: {
-      tempo: 500,
-      notes: [0, 5, 0, 4],
-      random: false,
-      tone: "low",
-    },
-    arp3: {
-      tempo: 1000,
-      notes: [1, 2, 3, 5],
-      random: true,
-      tone: "high",
-    },
-    arp4: {
-      tempo: 750,
-      notes: [6, 4, 1, 2],
-      random: false,
-      tone: "low",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 150,
-      notes: [1, 2, 6, 4, 0, 5, 4, 2, 3, 4, 6, 5, 4, 2, 3],
-      random: false,
-      tone: "high",
-    },
-    arp2: {
-      tempo: 600,
-      notes: [0, 0, 1, 5, 4, 4, 5, 2],
-      random: false,
-      tone: "low",
-    },
-    arp3: {
-      tempo: 600,
-      notes: [2, 2, 5, 6, 1, 0, 6, 5],
-      random: false,
-      tone: "low",
-    },
-    arp4: {
-      tempo: 0,
-      notes: [],
-      random: false,
-      tone: "high",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 200,
-      notes: [2, 2, 1, 6],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 400,
-      notes: [1, 1, 1, 1, 2, 5, 0, 3],
-      random: false,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 600,
-      notes: [2, 2, 3, 4, 6, 5, 4, 3],
-      random: false,
-      tone: "high",
-    },
-    arp4: {
-      tempo: 0,
-      notes: [-1],
-      random: false,
-      tone: "",
-    },
-  },
-
-  {
-    arp1: {
-      tempo: 200,
-      notes: [0, 0, 0, 2],
-      random: false,
-      tone: "full",
-    },
-    arp2: {
-      tempo: 200,
-      notes: [4, 6, 5, 4, 5, 4, 6, 3],
-      random: false,
-      tone: "full",
-    },
-    arp3: {
-      tempo: 200,
-      notes: [2, 0, 2, 0, 0, 2, 4, 4],
-      random: false,
-      tone: "full",
-    },
-    arp4: {
-      tempo: 800,
-      notes: [6, 4, 3, 2, 5, 4, 2, 5],
-      random: false,
-      tone: "high",
-    },
-  },
-  {
-    arp1: {
-      tempo: 200,
-      notes: [0, 2, 4, 0, 2, 4, 6, 0],
-      random: false,
-      tone: "low",
-    },
-    arp2: {
-      tempo: 200,
-      notes: [3, 4, 6, 3, 4, 5, 3, 4, 2, 3, 4, 5, 3, 4, 6, 1],
-      random: false,
-      tone: "high",
-    },
-    arp3: {
-      tempo: 200,
-      notes: [0, 0, 6, 0, 0, 0, 6, 0],
-      random: false,
-      tone: "low",
-    },
-    arp4: {
-      tempo: 1600,
-      notes: [6],
-      random: false,
-      tone: "full",
-    },
-  },
-];
-
-function displayDemos() {
-  demoBtns.innerHTML = "";
-  demos.forEach((demo) => {
-    const btn = document.createElement("BUTTON");
-    btn.classList.add("demoBtn");
-    btn.dataset.demo = demos.indexOf(demo);
-    btn.innerText = demos.indexOf(demo) + 1;
-    demoBtns.appendChild(btn);
+function addFocusEventListeners(unit) {
+  unit.querySelectorAll(".inputNote").forEach((el) => {
+    el.addEventListener("focus", (e) => {
+      currentNoteInput = e.target;
+    });
   });
 }
 
-function arpConfig(source) {
-  arpeggiate(
-    scale_C_Lydian,
-    source.arp1.tempo,
-    source.arp1.random,
-    source.arp1.tone,
-    source.arp1.notes
-  );
-  arpeggiate(
-    scale_C_Lydian,
-    source.arp2.tempo,
-    source.arp2.random,
-    source.arp2.tone,
-    source.arp2.notes
-  );
-  arpeggiate(
-    scale_C_Lydian,
-    source.arp3.tempo,
-    source.arp3.random,
-    source.arp3.tone,
-    source.arp3.notes
-  );
-  arpeggiate(
-    scale_C_Lydian,
-    source.arp4.tempo,
-    source.arp4.random,
-    source.arp4.tone,
-    source.arp4.notes
-  );
-}
+addFocusEventListeners(arp1);
 
-document.addEventListener("click", (e) => {
-  if (e.target.dataset.demo) {
-    arpConfig(demos[e.target.dataset.demo]);
-    codeDisplay.innerText =
-      e.target.innerText +
-      " => => => " +
-      JSON.stringify(demos[e.target.dataset.demo]);
-  } else if (e.target.id === "saveBtn" && !demos.includes(currConf)) {
-    demos.push(currConf);
-    displayDemos();
-    localStorage.setItem("demos", JSON.stringify(demos));
+units.addEventListener("click", (e) => {
+  if (e.target.classList.contains("removeUnitBtn")) {
+    e.target.parentElement.remove();
+  }
+
+  if (e.target.dataset.note) {
+    currentNoteInput.value =
+      e.target.dataset.note + e.target.parentElement.dataset.range;
+
+    currentNoteInput.nextElementSibling.focus();
   }
 });
 
-if (localStorage.getItem("demos")) {
-  demos = JSON.parse(localStorage.getItem("demos"));
-  console.log(demos);
+function playGroup(obj) {
+  obj.forEach((unit) => {
+    playSequence(unit);
+  });
 }
 
-displayDemos();
+let demos = [
+  [
+    {
+      n: "1",
+      tempo: 210,
+      notes: ["E2", "G2", "B2", "E2", "G3", "B2"],
+      random: false,
+    },
+    {
+      n: "2",
+      tempo: 420,
+      notes: ["B2", "A2", "G2", "A2", "C2"],
+      random: false,
+    },
+    {
+      n: "3",
+      tempo: 630,
+      notes: ["B3", " ", "E3", "A3", "C3", " ", "D3", "E3"],
+      random: false,
+    },
+    {
+      n: "4",
+      tempo: 210,
+      notes: ["C2", "G2", "E2", "A2", "G2"],
+      random: false,
+    },
+  ],
+];
+
+// playGroup(demos[0]);
+playDemo1.addEventListener("click", () => {
+  playGroup(demos[0]);
+});
